2e8d9be493543365a29ed0a4a25dfdc38e3162aa
LijingWang <lijing@ualberta.ca>
2021-04-08 20:34:32 -0600

    added async and sync unit tests

--- LHL-w2d2/cats/asyncBreeds.js
+++ LHL-w2d2/cats/asyncBreeds.js
@@ -1,12 +1,14 @@
 // asyncBreeds.js
 const fs = require('fs');
+const breedDetails = require('./syncBreeds');
 
 const breedDetailsFromFile = function(breed, callback) {
-  console.log('breedDetailsFromFile: Calling readFile...');
+  // console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
-    console.log("In readFile's Callback: it has the data.");
+    // console.log("In readFile's Callback: it has the data.");
     // ISSUE: Returning from *inner* callback function, not breedDetailsFromFile.
     if (!error) return callback(data);
+    return callback(undefined);
   });
   // ISSUE: Attempting to return data out here will also not work.
   //        Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
@@ -16,5 +18,4 @@ const Done = (data) => {
     console.log('Return Value: ', data); 
 }
 
-// we try to get the return value
-const bombay = breedDetailsFromFile('Bombay', Done);
+module.exports = breedDetailsFromFile;
